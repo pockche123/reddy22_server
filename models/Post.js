94b358baf6c50cea91723a26920e46bfffb981cb
nine96as <ashrafsaid79@gmail.com>
2023-11-27 13:35:35 +0000

    Fill in post, token, user model logic

---
+++ models/Post.js
@@ -0,0 +1,46 @@
+const db = require('../database/connect');
+
+class Post {
+  constructor({ post_id, user_id, title, content }) {
+    this.id = post_id;
+    this.user_id = user_id;
+    this.title = title;
+    this.content = content;
+  }
+
+  static async getAll() {
+    const response = await db.query('SELECT * FROM post');
+    return response.rows.map((p) => new Post(p));
+  }
+
+  static async getOneById(id) {
+    const response = await db.query('SELECT * FROM post WHERE post_id = $1', [
+      id
+    ]);
+    if (response.rows.length != 1) {
+      throw new Error('Unable to locate post.');
+    }
+    return new Post(response.rows[0]);
+  }
+
+  static async create(data) {
+    const { title, content, user_id } = data;
+    let response = await db.query(
+      'INSERT INTO post (title, content, user_id) VALUES ($1, $2, $3) RETURNING post_id;',
+      [title, content, user_id]
+    );
+    const newId = response.rows[0].post_id;
+    const newPost = await Post.getOneById(newId);
+    return newPost;
+  }
+
+  async destroy() {
+    let response = await db.query(
+      'DELETE FROM post WHERE post_id = $1 RETURNING *;',
+      [this.id]
+    );
+    return new Post(response.rows[0]);
+  }
+}
+
+module.exports = Post;
