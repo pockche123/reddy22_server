a1b9d4e2896ce7f235c652c56970299f9db865ad
nine96as <nine96@skiff.com>
2023-11-27 15:54:17 +0000

    Add scores table

--- database/setup.sql
+++ database/setup.sql
@@ -51,6 +51,15 @@ CREATE TABLE materials (
     FOREIGN KEY (bin_id) REFERENCES bins(bin_id)
 );
 
+CREATE TABLE scores (
+    score_id INT GENERATED ALWAYS AS IDENTITY,
+    user_id INT NOT NULL,
+    category_id INT NOT NULL,
+    value INT NOT NULL,
+    PRIMARY KEY (token_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+)
+
 INSERT INTO bins (bin_type, color, bin_image, info) VALUES
     ('Recycling collection', 'blue', 'https://www.warwickdc.gov.uk/images/Recycing_bin_1.jpg', 'Your 240 litre recycling bin will be collected every fortnight - check the collection calendar for your collection day.'),
     ('Refuge collection', 'grey', 'https://www.warwickdc.gov.uk/images/Waste_bin_1.jpg', 'The grey bin is collected every three weeks.\nPlease use your grey bin for household items that can not be recycled. All rubbish must be contained in the grey bin with the lid firmly closed. Bags of rubbish left anywhere around the bin will not be collected. Any extra rubbish can be taken to a Household Waste Recycling Centre.'),
