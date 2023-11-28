a7bae95e2ae40d0f6997914f976647e7a73e82ba
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-11-28 12:52:22 +0000

    add show on bin

--- controllers/binController.js
+++ controllers/binController.js
@@ -10,4 +10,20 @@ const index = async (req, res) => {
   }
 };
 
-module.exports = { index };
+const show = async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  try {
+    const bin = await Bin.findById(id);
+
+    if (!bin) {
+      return res.status(404).json({ error: 'bin not found' });
+    }
+    return res.status(200).json(bin);
+  } catch (error) {
+    console.error('Error retrieving bin:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+module.exports = { index, show };
